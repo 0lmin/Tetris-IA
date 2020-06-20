a299d02da70d1ecdec1a33b849e1b69549b3962d
Bertin Clement <Clement.Bertin@bordeaux-inp.fr>
2020-06-20 21:43:23 +0200

    fix(Tetromino): fix matrices

--- Tetromino.py
+++ Tetromino.py
@@ -1,4 +1,3 @@
-import sys, pygame
 import numpy as np
 
 T_TYPE=0
@@ -28,20 +27,73 @@ TETROMINO_I = ["I", (0, 255, 255), [np.array([[0,0,0,0],
                                                                                                                    [0,0,1,0],
                                                                                                                    [0,0,1,0],
                                                                                                                    [0,0,1,0]])]]
-TETROMINO_O = ["O", (255, 255, 0)]
-TETROMINO_T = ["T", (170, 0, 255)]
-TETROMINO_L = ["L", (255, 165, 0)]
-TETROMINO_J = ["J", (0, 0, 255)]
-TETROMINO_Z = ["Z", (255, 0, 0)]
-TETROMINO_S = ["S", (0, 255, 0)]
+TETROMINO_O = ["O", (255, 255, 0), [np.array([[0,1,1,0],
+                                              [0,1,1,0],
+                                              [0,0,0,0],
+                                              [0,0,0,0]]), np.array([[0,1,1,0],
+                                                                     [0,1,1,0],
+                                                                     [0,0,0,0],
+                                                                     [0,0,0,0]]), np.array([[0,1,1,0],
+                                                                                            [0,1,1,0],
+                                                                                            [0,0,0,0],
+                                                                                            [0,0,0,0]]), np.array([[0,1,1,0],
+                                                                                                                   [0,1,1,0],
+                                                                                                                   [0,0,0,0],
+                                                                                                                   [0,0,0,0]])]]
+TETROMINO_T = ["T", (170, 0, 255), [np.array([[0,1,0],
+                                              [1,1,1],
+                                              [0,0,0]]),np.array([[0,1,0],
+                                                                 [0,1,1],
+                                                                 [0,1,0]]), np.array([[0,0,0],
+                                                                                      [1,1,1],
+                                                                                      [0,1,0]]), np.array([[0,1,0],
+                                                                                                           [1,1,0],
+                                                                                                           [0,1,0]])]]
+TETROMINO_L = ["L", (255, 165, 0), [np.array([[0,0,1],
+                                              [1,1,1],
+                                              [0,0,0]]),np.array([[0,1,0],
+                                                                 [0,1,0],
+                                                                 [0,1,1]]), np.array([[0,0,0],
+                                                                                      [1,1,1],
+                                                                                      [1,0,0]]), np.array([[1,1,0],
+                                                                                                           [0,1,0],
+                                                                                                           [0,1,0]])]]
+TETROMINO_J = ["J", (0, 0, 255), [np.array([[1,0,0],
+                                            [1,1,1],
+                                            [0,0,0]]),np.array([[0,1,1],
+                                                               [0,1,0],
+                                                               [0,1,0]]), np.array([[0,0,0],
+                                                                                    [1,1,1],
+                                                                                    [0,0,1]]), np.array([[0,1,0],
+                                                                                                         [0,1,0],
+                                                                                                         [1,1,0]])]]
+TETROMINO_Z = ["Z", (255, 0, 0), [np.array([[1,1,0],
+                                            [0,1,1],
+                                            [0,0,0]]),np.array([[0,0,1],
+                                                               [0,1,1],
+                                                               [0,1,0]]), np.array([[0,0,0],
+                                                                                    [1,1,0],
+                                                                                    [0,1,1]]), np.array([[0,1,0],
+                                                                                                         [1,1,0],
+                                                                                                         [1,0,0]])]]
+TETROMINO_S = ["S", (0, 255, 0), [np.array([[0,1,1],
+                                            [1,1,0],
+                                            [0,0,0]]),np.array([[0,1,0],
+                                                                 [0,1,1],
+                                                                 [0,0,1]]), np.array([[0,0,0],
+                                                                                      [0,1,1],
+                                                                                      [1,1,0]]), np.array([[1,0,0],
+                                                                                                           [1,1,0],
+                                                                                                           [0,1,0]])]]
 TETROMINOS = {"I":TETROMINO_I, "O":TETROMINO_O, "T":TETROMINO_T, "L":TETROMINO_L, "J":TETROMINO_J, "Z":TETROMINO_Z, "S":TETROMINO_S}
 
 class Tetromino:
     def __init__(self, type, rotation, position):
-        self.color = TETROMINOS[type][T_COLOR] # TETROMINOS[type].color TETROMINOS.get(type)[T_COLOR]
+        self.color = TETROMINOS.get(type)[T_COLOR] # TETROMINOS[type].color TETROMINOS.get(type)[T_COLOR]
         self.type = type #between 0 and 7
         self.position = position #topleft corner
-        self.matrice = TETROMINOS[type][T_MATRICE][rotation] #matrice
+        self.color = TETROMINOS.get(type)[T_COLOR]
+        self.matrice = TETROMINOS.get(type)[T_MATRICE][rotation] #matrice
 
 
     def applyOnBoard(self, board):#fonction appelé à chaque (affichage) + placement de la piece
