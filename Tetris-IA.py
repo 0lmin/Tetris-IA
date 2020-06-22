4d71ea16549107584685d913e99100f861ec2596
Axel CHEMIN <axel.chemin@polytech-lille.net>
2020-06-22 09:34:36 +0200

    feat(game GUI): switch to np array for board

--- Tetris-IA.py
+++ Tetris-IA.py
@@ -82,7 +82,7 @@ while 1:
     # Draw board
     ## To be implemented after GUI
     ##board = [([0] * 21)] * 11 #BUG => same ref, deepcopy needed?
-    board = np.zeros((10, 20), np.int8)#access via board[x][y]
+    board = np.zeros((10, 20, 3), np.int8)#access via board[x][y]
     board[0][2] = 1 #0 empty, [1;7] => colors
     board[1][3] = 2
     board[1][4] = 2
